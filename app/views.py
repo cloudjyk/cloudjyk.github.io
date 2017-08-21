4f16fd2667f23c7ec0e9c6eb6ae20b3f684ebb67
cloudjyk <cloudjyk@163.com>
2017-08-21 21:23:30 +0800

    2nd day block used

--- app/views.py
+++ app/views.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from app import app
+from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    # return 'Hello World!'
+    user = {'nickname':'Cloud'}
+    posts = [{'author': { 'nickname': 'John' },'body': 'I\'m the 1st one !Beautiful day in Portland!'},
+    {'author': { 'nickname': 'Susan' },'body': 'I\'m No.2!The Avengers movie was so cool!'}]
+    return render_template('index.html', title = 'Home', user = user, posts = posts)
+
+
+
+    # return '''
+    # <html>
+    #     <head>
+    #         <title>Home Page</title>
+    #     </head>
+    #     <body>
+    #         <h1>hello1,'''+user['nickname']+'''</h1>
+    #         <h2>hello2,'''+user['nickname']+'''</h2>
+    #     </body>
+    # </html>'''
