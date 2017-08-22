ad30c05261c8545551c4912622e607e4409c9530
cloudjyk <cloudjyk@163.com>
2017-08-22 22:01:43 +0800

    4th day db initialed

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# config initialed
 from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
-from app import views
+
+# db initialed
+from flask.ext.sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
+
+from app import views, models
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# OpenID 配置
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 OPENID_PROVIDERS = [
@@ -6,3 +7,9 @@ OPENID_PROVIDERS = [
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+# 引入数据库
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
